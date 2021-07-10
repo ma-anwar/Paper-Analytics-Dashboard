e794d52626775605b1865c78786227758a53c5b4
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-07-10 14:56:10 -0400

    Add server

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import pandas as pd
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 
 df = pd.read_csv("data/sales_data.csv", parse_dates=True)
 
